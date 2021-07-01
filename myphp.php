76f64d55d9a8f10f51d8d33833e978874572150f
winni_lina <9811011sa01@gmail.com>
2021-07-01 07:14:33 +0000

    PHP 실습

---
+++ myphp.php
@@ -0,0 +1,20 @@
+<?php
+include('showerror.php');
+?>
+HTML 영역
+print("Hello");
+<?php
+print("Hello\n");
+if(false){
+	pridnt("Hello\n");
+	print("Hello");
+	pridnt("Hello\n");
+}
+
+$x = 3;
+$x = "무야호" ;
+$value = 1000000000000;
+print $value."<br>";
+$value = $value + 1;
+print $value."<br>";
+?>
